4caeb9153fdc10a339c6a0ec46142db9942ecf01
parteek kumar <digitalparteek@gmail.com>
2023-07-06 13:26:58 +0530

    Navbar initilized

--- components/Nav.js
+++ components/Nav.js
@@ -1,23 +1,46 @@
-//  links
-// const links = [
-//   { name: 'home', path: '/', icon: <HiHome /> },
-//   { name: 'about', path: '/about', icon: <HiUser /> },
-//   { name: 'services', path: '/services', icon: <HiRectangleGroup /> },
-//   { name: 'work', path: '/work', icon: <HiViewColumns /> },
-//   {
-//     name: 'testimonials',
-//     path: '/testimonials',
-//     icon: <HiChatBubbleBottomCenterText />,
-//   },
-//   {
-//     name: 'contact',
-//     path: '/contact',
-//     icon: <HiEnvelope />,
-//   },
-// ];
+// icons importing
+import {
+  HiHome,
+  HiUser,
+  HiRectangleGroup,
+  HiViewColumns,
+  HiChatBubbleBottomCenterText,
+  HiEnvelope,
+} from "react-icons/hi2";
+
+//  navigation data
+export const navData = [
+  { name: "home", path: "/", icon: <HiHome /> },
+  { name: "about", path: "/about", icon: <HiUser /> },
+  { name: "services", path: "/services", icon: <HiRectangleGroup /> },
+  { name: "work", path: "/work", icon: <HiViewColumns /> },
+  {
+    name: "testimonials",
+    path: "/testimonials",
+    icon: <HiChatBubbleBottomCenterText />,
+  },
+  {
+    name: "contact",
+    path: "/contact",
+    icon: <HiEnvelope />,
+  },
+];
+
+// next link
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Nav = () => {
-  return <nav>nav</nav>;
+  return (
+    <nav>
+      <div>
+        {/* inner */}
+        {navData.map((link, index) => {
+          return <Link href={link.path}>{link.icon}</Link>;
+        })}
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
