6fa9d2778427970f59505c058c76235dcf713a31
parteek kumar <digitalparteek@gmail.com>
2023-07-06 17:12:49 +0530

    Added avatar

--- pages/index.js
+++ pages/index.js
@@ -23,26 +23,58 @@ const Home = () => {
       <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
         <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
           {/* title */}
-          <h1 className="h1">
+          <motion.h1
+            variants={fadeIn("down", 0.2)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="h1"
+          >
             Transforming Ideas <br /> Into{" "}
             <span className="text-accent"> Digital Reality</span>
-          </h1>
+          </motion.h1>
           {/* subtitle */}
-          <p className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16">
+          <motion.p
+            variants={fadeIn("down", 0.3)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16"
+          >
             Minim consequat voluptate dolor ut sint Lorem officia mollit ea
             dolor ea proident amet laboris. Duis tempor tempor officia duis
             irure .
-          </p>
+          </motion.p>
 
           {/* btns */}
           <div className="flex justify-center xl:hidden relative">
             <ProjectsBtn />
           </div>
+          <motion.div
+            variants={fadeIn("down", 0.4)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="hidden xl:flex"
+          >
+            <ProjectsBtn />
+          </motion.div>
         </div>
       </div>
 
       {/* image */}
-      <div>image</div>
+      <div className="w-[1200px] h-full absolute right-0 bottom-0">
+        {/* bg image */}
+        <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"></div>
+        <div>
+          {/* particles */}
+          <div>Particles</div>
+          {/* avatar image */}
+          <div className="w-full h-full max-w-[587px] max-h-[528px] absolute -bottom-32  lg:bottom-0 lg:right-[5%]">
+            <Avatar />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
