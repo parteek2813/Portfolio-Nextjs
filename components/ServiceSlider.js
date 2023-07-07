261538a548c104bd2ccc788c5736725bb9ec02b7
parteek kumar <digitalparteek@gmail.com>
2023-07-07 15:41:37 +0530

    swiper setup

--- components/ServiceSlider.js
+++ components/ServiceSlider.js
@@ -1,3 +1,21 @@
+// import swiper react componenets
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// import swiper styles
+import "swiper/css";
+import "swiper/css/free-made";
+import "swiper/css/pagination";
+
+// icons
+import {
+  RxCrop,
+  RxDesktop,
+  RxPencil2,
+  RxReader,
+  RxRocket,
+  RxArrowTopRight,
+} from "react-icons/rx";
+
 const ServiceSlider = () => {
   return <div>Service Slider</div>;
 };
