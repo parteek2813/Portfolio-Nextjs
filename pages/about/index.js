4cc90980b5bc6b1aba99b50ef99ffb2504d53de6
parteek kumar <digitalparteek@gmail.com>
2023-07-07 13:13:24 +0530

    About page setup

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,3 +1,29 @@
+import React, { useState } from "react";
+
+import {
+  FaHtml5,
+  FaCss3,
+  FaJs,
+  FaReact,
+  FaJava,
+  FaNodeJs,
+} from "react-icons/fa";
+
+import {
+  SiNextdotjs,
+  SiFramer,
+  SiAdobexd,
+  SiAdobephotoshop,
+} from "react-icons/si";
+
+// componenets
+import Avatar from "../../components/Avatar";
+import Circles from "../../components/Circles";
+
+// framer motion
+import { motion } from "framer-motion";
+import { fadeIn } from "../../variants";
+
 //  data
 const aboutData = [
   {
@@ -12,12 +38,12 @@ const aboutData = [
           <FaReact />,
           <SiNextdotjs />,
           <SiFramer />,
-          <FaWordpress />,
+          <FaJava />,
         ],
       },
       {
         title: "UI/UX Design",
-        icons: [<FaFigma />, <SiAdobexd />, <SiAdobephotoshop />],
+        icons: [<FaNodeJs />, <SiAdobexd />, <SiAdobephotoshop />],
       },
     ],
   },
@@ -71,7 +97,44 @@ const aboutData = [
 ];
 
 const About = () => {
-  return <div>About</div>;
+  const [index, setIndex] = useState(0);
+  console.log(index);
+  return (
+    <div className="h-full bg-primary/30 py-32 text-center xl:text-left  ">
+      <Circles />
+      {/* avatar image */}
+      <motion.div
+        variants={fadeIn("right", 0.2)}
+        initial="hidden"
+        animate="show"
+        exit="hidden"
+        className="hidden xl:flex absolute bottom-0 -left-[370px]"
+      >
+        <Avatar />
+      </motion.div>
+      <div className="conatiner mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6">
+        <div>text</div>
+        <div>
+          <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
+            {aboutData.map((item, itemIndex) => {
+              return (
+                <div
+                  key={itemIndex}
+                  className={` ${
+                    index === itemIndex &&
+                    "text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300"
+                  }  cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:bottom-1 after:left-0`}
+                  onClick={() => setIndex(itemIndex)}
+                >
+                  {item.title}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
