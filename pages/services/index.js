885b331eb1ef7d528cc68fab170d5fbf53a53b7f
parteek kumar <digitalparteek@gmail.com>
2023-07-07 15:33:24 +0530

    Added code*

--- pages/services/index.js
+++ pages/services/index.js
@@ -1,5 +1,74 @@
+// icons
+import {
+  RxCrop,
+  RxPencil2,
+  RxDesktop,
+  RxReader,
+  RxRocket,
+} from "react-icons/rx";
+
+// components
+import ServiceSlider from "../../components/ServiceSlider";
+import Bulb from "../../components/Bulb";
+import Circles from "../../components/Circles";
+
+// framer motion
+import { motion } from "framer-motion";
+import { fadeIn } from "../../variants";
+
+const serviceData = [
+  {
+    icon: <RxCrop />,
+    title: "Branding",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    icon: <RxPencil2 />,
+    title: "Design",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    icon: <RxDesktop />,
+    title: "Development",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    icon: <RxReader />,
+    title: "Copywriting",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    icon: <RxRocket />,
+    title: "SEO",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+];
+
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <div className="h-full bg-primary/30 py-36 flex items-center">
+      <Circles />
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-x-0">
+          {/* text */}
+          <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
+            <h2 className="h2 xl:mt-8 ">
+              My services <span className="text-accent"></span>
+            </h2>
+            <p className="mb-4 max-w-[400px] mx-auto lg:mx-0">
+              Veniam mollit culpa Lorem non quis exercitation laborum anim
+              proident. Laboris quis consectetur ad fugiat esse dolore.
+            </p>
+          </div>
+          {/* slider */}
+          <div>
+            <ServiceSlider />
+          </div>
+        </div>
+      </div>
+      <Bulb />
+    </div>
+  );
 };
 
 export default Services;
