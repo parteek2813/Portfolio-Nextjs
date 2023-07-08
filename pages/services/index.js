7a54390dbbbbf271ac88d3a446fb51bcd931ca84
parteek kumar <digitalparteek@gmail.com>
2023-07-08 13:45:33 +0530

    services page complete

--- pages/services/index.js
+++ pages/services/index.js
@@ -1,12 +1,3 @@
-// icons
-import {
-  RxCrop,
-  RxPencil2,
-  RxDesktop,
-  RxReader,
-  RxRocket,
-} from "react-icons/rx";
-
 // components
 import ServiceSlider from "../../components/ServiceSlider";
 import Bulb from "../../components/Bulb";
@@ -16,34 +7,6 @@ import Circles from "../../components/Circles";
 import { motion } from "framer-motion";
 import { fadeIn } from "../../variants";
 
-const serviceData = [
-  {
-    icon: <RxCrop />,
-    title: "Branding",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-  },
-  {
-    icon: <RxPencil2 />,
-    title: "Design",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-  },
-  {
-    icon: <RxDesktop />,
-    title: "Development",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-  },
-  {
-    icon: <RxReader />,
-    title: "Copywriting",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-  },
-  {
-    icon: <RxRocket />,
-    title: "SEO",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-  },
-];
-
 const Services = () => {
   return (
     <div className="h-full bg-primary/30 py-36 flex items-center">
@@ -52,18 +15,36 @@ const Services = () => {
         <div className="flex flex-col xl:flex-row gap-x-0">
           {/* text */}
           <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
-            <h2 className="h2 xl:mt-8 ">
+            <motion.h2
+              variants={fadeIn("up", 0.2)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="h2 xl:mt-8 "
+            >
               My services <span className="text-accent"></span>
-            </h2>
-            <p className="mb-4 max-w-[400px] mx-auto lg:mx-0">
+            </motion.h2>
+            <motion.p
+              variants={fadeIn("up", 0.4)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="mb-4 max-w-[400px] mx-auto lg:mx-0"
+            >
               Veniam mollit culpa Lorem non quis exercitation laborum anim
               proident. Laboris quis consectetur ad fugiat esse dolore.
-            </p>
+            </motion.p>
           </div>
           {/* slider */}
-          <div>
+          <motion.div
+            variants={fadeIn("down", 0.6)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="w-full xl:max-w-[65%]"
+          >
             <ServiceSlider />
-          </div>
+          </motion.div>
         </div>
       </div>
       <Bulb />
