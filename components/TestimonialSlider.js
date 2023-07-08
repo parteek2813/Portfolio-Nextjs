156356ae6b6bb58179b8f44af1e752ca316e28dd
parteek kumar <digitalparteek@gmail.com>
2023-07-08 14:41:28 +0530

    initialized testimonial page

--- components/TestimonialSlider.js
+++ components/TestimonialSlider.js
@@ -1,30 +1,81 @@
 // testimonial data
-const testimonialData = [
+export const testimonialData = [
   {
-    image: '/t-avt-1.png',
-    name: 'Anne Smith',
-    position: 'Customer',
+    image: "/t-avt-1.png",
+    name: "Anne Smith",
+    position: "Customer",
     message:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!",
   },
   {
-    image: '/t-avt-2.png',
-    name: 'Jane Doe',
-    position: 'Customer',
+    image: "/t-avt-2.png",
+    name: "Jane Doe",
+    position: "Customer",
     message:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!",
   },
   {
-    image: '/t-avt-3.png',
-    name: 'Jhon Doe',
-    position: 'Customer',
+    image: "/t-avt-3.png",
+    name: "Jhon Doe",
+    position: "Customer",
     message:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!",
   },
 ];
 
-const TestimonialSlider = () => {
-  return <div>Testimonial Slider</div>;
+// next image
+import Image from "next/image";
+
+// import swiper react componenets
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// import swiper styles
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+// import required modules
+import { Pagination } from "swiper";
+
+//icons
+import { BsArrowRight } from "react-icons/bs";
+
+const WorkSlider = () => {
+  return (
+    <Swiper
+      spaceBetween={10}
+      pagination={{
+        clickable: true,
+      }}
+      modules={{ Pagination }}
+      className="h-[280px] sm:h-[480px]"
+    >
+      {testimonialData.map((person, index) => {
+        return (
+          <SwiperSlide key={index}>
+            <div>
+              {/*  avatar , name, postion*/}
+              <div>
+                <div>
+                  {/* avatar  */}
+                  <div>avatar image</div>
+                  {/* name  */}
+                  <div>name</div>
+                  {/* position  */}
+                  <div>position</div>
+                </div>
+              </div>
+              <div>
+                {/* quote & messages */}
+                <div>quote & messages</div>
+              </div>
+            </div>
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
 };
 
-export default TestimonialSlider;
+export default WorkSlider;
