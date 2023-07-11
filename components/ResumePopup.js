5384d7a5377383e6dd5c1b3fcd0b4626a586b80a
parteek kumar <digitalparteek@gmail.com>
2023-07-11 15:24:12 +0530

    fixed popup res

--- components/ResumePopup.js
+++ components/ResumePopup.js
@@ -3,17 +3,16 @@ import Image from "next/image";
 import styles from "../styles/ResumePopup.module.css";
 
 const ResumePopup = ({ onClose }) => {
-  // console.log(onClose);
+  const handleClose = () => {
+    onClose();
+  };
   return (
     <div className={styles.overlay} style={{ zIndex: 1 }}>
       <div className={styles.popup}>
         <div className={styles.resumeContainer}>
           <Image src="/resume.png" width={800} height={1000} alt="Resume" />
         </div>
-        <button
-          className={styles.closeButton}
-          onClick={() => console.log("clicked")}
-        >
+        <button className={styles.closeButton} onClick={handleClose}>
           &times;
         </button>
       </div>
