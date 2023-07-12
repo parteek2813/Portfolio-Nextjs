a9e181eb087a1eb00f5d4f2d808e7e7757738ca1
parteek kumar <digitalparteek@gmail.com>
2023-07-12 18:45:49 +0530

    disabled eslint

--- next.config.js
+++ next.config.js
@@ -18,6 +18,9 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
